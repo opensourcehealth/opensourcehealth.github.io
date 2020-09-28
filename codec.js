4d0ea995492923ab59b03cb5b5b953d8069bacd8
opensourcehealth <70734458+opensourcehealth@users.noreply.github.com>
2020-09-28 00:45:01 -0700

    Add files via upload

---
+++ codec.js
@@ -0,0 +1,158 @@
+//License = GNU Affero General Public License http://www.gnu.org/licenses/agpl.html
+
+var gBackupNumberOfUpdates = -1
+var gBracketTable = {
+	'&lt;':'<',
+	'&gt;':'>'}
+var gEscapeTable = {
+	'%0A':'\n',
+	'%0D':'\r',
+	'%20':' ',
+	'%24':'$',
+	'%26':'&',
+	'%60':'`',
+	'%3A':':',
+	'%3C':'<',
+	'%3E':'>',
+	'%5B':'[',
+	'%5D':']',
+	'%7B':'{',
+	'%7D':'}',
+	'%22':'"',
+	'%2B':'+',
+	'%23':'#',
+	'%25':'%',
+	'%40':'@',
+	'%2F':'/',
+	'%3B':';',
+	'%3D':'=',
+	'%3F':'?',
+	'%5C':'\\',
+	'%5E':'^',
+	'%7C':'|',
+	'%7E':'~',
+	'%27':'\'',
+	'%2C':','}
+var gBracketExpression = new RegExp(Object.keys(gBracketTable).join("|"), "gi")
+var gEscapeExpression =	new RegExp(Object.keys(gEscapeTable).join("|"), "gi")
+var gQueryMap = new Map()
+
+function getBracketReplacedLines(textAreaID) {
+	var wordString = document.getElementById(textAreaID).value
+	wordString = wordString.replace(gBracketExpression, function(find) {return gBracketTable[find]})
+	var splitWord = '\n'
+	if (wordString.indexOf('\r\n') > -1) {
+		splitWord = '\r\n'
+	}
+	return wordString.split(splitWord)
+}
+
+function querySelectChanged() {
+	var querySelect = document.getElementById('querySelect')
+	var queryKey = querySelect.options[querySelect.selectedIndex].text
+	if (gQueryMap.has(queryKey)) {
+		var queryLink = document.getElementById('queryLink')
+		queryLink.href = '?' + gQueryMap.get(queryKey)
+		queryLink.innerHTML = 'Go'
+	}
+}
+
+function setNumberOfRows(id, lines) {
+	var numberOfRows = 1
+	var oneOverColumns = 1.0 / document.getElementById(id).cols
+	for (line of lines) {
+		numberOfRows += Math.ceil(oneOverColumns * (line.length + 1))
+	}
+	document.getElementById(id).rows = numberOfRows
+}
+
+function setQueryStorage(newButtonID, querySelectID, wordString) {
+	var date = new Date()
+	var queryTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+	var queryLink = document.getElementById('queryLink')
+	var queryValue = wordString.replace(/\n/g, '%0A').replace(/,/g, '%2C')
+	if (typeof(Storage) == "undefined" || querySelectID == null) {
+		gBackupNumberOfUpdates += 1
+		if (gBackupNumberOfUpdates > 0) {
+			queryLink.hidden = false
+			queryLink.href = '?' + queryValue
+			queryLink.innerHTML = 'Update ' + gBackupNumberOfUpdates.toString() + ' - ' + queryTime
+		}
+		return
+	}
+	var queries = []
+	var query = queryTime + ';' + queryValue
+	if (sessionStorage.queries) {
+		queries = sessionStorage.queries.split(',')
+	}
+	var identicalKey = null
+	for (queryIndex = 0; queryIndex < queries.length; queryIndex++) {
+		storageQuery = queries[queryIndex]
+		var indexOfSemicolon = storageQuery.indexOf(';')
+		if (indexOfSemicolon != -1) {
+			if (storageQuery.slice(indexOfSemicolon + 1) == queryValue) {
+				identicalKey = storageQuery.slice(0, indexOfSemicolon)
+				queryIndex = queries.length
+			}
+		}
+	}
+	if (identicalKey == null) {
+		query = queries.length.toString() + ' - ' + query
+		queries.push(query)
+		var indexOfSemicolon = query.indexOf(';')
+		var identicalKey = query.slice(0, indexOfSemicolon)
+	}
+	sessionStorage.queries = queries.toString()
+	var querySelect = document.getElementById(querySelectID)
+	if (queries.length > 1) {
+		if (newButtonID != null) {
+			document.getElementById(newButtonID).hidden = false
+		}
+		queryLink.hidden = false
+		querySelect.hidden = false
+	}
+	for (query of queries) {
+		var indexOfSemicolon = query.indexOf(';')
+		var key = query.slice(0, indexOfSemicolon)
+		if (!gQueryMap.has(key)) {
+			var option = document.createElement('option')
+			option.text = key
+			querySelect.add(option)
+			gQueryMap.set(key, query.slice(indexOfSemicolon + 1))
+		}
+	}
+	var options = querySelect.options
+	for (optionIndex = 0; optionIndex < options.length; optionIndex++) {
+		if (options[optionIndex].text == identicalKey) {
+			querySelect.selectedIndex = optionIndex
+			queryLink.href = '?' + gQueryMap.get(identicalKey)
+			queryLink.innerHTML = 'Go'
+			optionIndex = options.length
+		}
+	}
+}
+
+function setTextArea(textAreaID) {
+	var query = document.URL
+	var indexOfQuestionMark = query.indexOf('?')
+	if (indexOfQuestionMark < 0) {
+		query = ''
+	}
+	else {
+		query = query.slice(indexOfQuestionMark + 1)
+	}
+	query = query.replace(gEscapeExpression, function(find) {return gEscapeTable[find]})
+	var indexOfNewline = query.indexOf('\n')
+	if (indexOfNewline < 0) {
+		query = query.replace(/></g, '>\n<')
+	}
+	document.getElementById(textAreaID).value = query
+}
+
+function windowNew() {
+	var querySelect = document.getElementById('querySelect')
+	var queryKey = querySelect.options[querySelect.selectedIndex].text
+	if (gQueryMap.has(queryKey)) {
+		window.open('?' + gQueryMap.get(queryKey))
+	}
+}
