7be8acd7e992627d3d9520b875a8eda0e758b788
opensourcehealth <70734458+opensourcehealth@users.noreply.github.com>
2020-09-28 00:45:18 -0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,26 @@
+//License = GNU Affero General Public License http://www.gnu.org/licenses/agpl.html
+
+var gAlertSet = new Set()
+
+function warning(text, variables) {
+	var textLines = text.split('\n')
+	var maximumLength = Math.max(textLines.length, variables.length)
+	for (itemIndex = 0; itemIndex < maximumLength; itemIndex++) {
+		if (itemIndex < textLines.length) {
+			console.log(textLines[itemIndex])
+		}
+		if (itemIndex < variables.length) {
+			console.log(variables[itemIndex])
+		}
+	}
+	if (gAlertSet.has(text)) {
+		return
+	}
+	gAlertSet.add(text)
+	var alertText = text
+	for (variable of variables) {
+		text += '\n' + variable
+	}
+	text += '\n\nFurther warnings about this will be sent to the debugging console only.'
+	alert(text)
+}
