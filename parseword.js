ac8cfe882d6f4c64dae2b549362d9ad1934bb8b9
opensourcehealth <70734458+opensourcehealth@users.noreply.github.com>
2020-10-13 17:33:07 -0700

    Update parseword.js

--- parseword.js
+++ parseword.js
@@ -1,5 +1,13 @@
 //License = GNU Affero General Public License http://www.gnu.org/licenses/agpl.html
 
+function addSliceToWords(lineSlice, quoteSeparatedWords) {
+	if (lineSlice.indexOf('=') != -1) {
+		lineSlice = getLineWithEndspace(['/>', '>', '[', '{'], lineSlice)
+	}
+	snippets = lineSlice.replace(/=/g, ' = ').split(' ').filter(lengthCheck)
+	pushArray(quoteSeparatedWords, snippets)
+}
+
 function getAttributes(tokens) {
 	var attributes = []
 	var key = null
@@ -34,6 +42,55 @@ function getJoinWord() {
 		return '\r\n'
 	}
 	return '\n'
+}
+
+function getLineWithEndspace(endWords, line) {
+	for (endWord of endWords) {
+		if (line.endsWith(endWord)) {
+			spaceThenEndWord = ' ' + endWord
+			if (!line.endsWith(spaceThenEndWord)) {
+				return line.slice(0, line.length - endWord.length) + spaceThenEndWord
+			}
+		}
+	}
+	return line
+}
+
+function getQuoteSeparatedWords(line) {
+	var quoteSymbols = ['"', '\'']
+	var searchIndexes = [-1,-1]
+	var start = 0
+	var quoteSeparatedWords = []
+	for (whileIndex = 0; whileIndex < 9876543; whileIndex++) {
+		for (quoteIndex = quoteSymbols.length -1; quoteIndex > -1; quoteIndex--) {
+			searchIndex = line.indexOf(quoteSymbols[quoteIndex], start)
+			if (searchIndex < 0) {
+				searchIndexes.splice(quoteIndex, 1)
+				quoteSymbols.splice(quoteIndex, 1)
+			}
+			else {
+				searchIndexes[quoteIndex] = searchIndex
+			}
+		}
+		if (quoteSymbols.length == 0) {
+			addSliceToWords(line.slice(start), quoteSeparatedWords)
+			return quoteSeparatedWords
+		}
+		firstIndex = searchIndexes[0]
+		quoteSymbol = quoteSymbols[0]
+		if (searchIndexes[1] < firstIndex) {
+			firstIndex = searchIndexes[1]
+			quoteSymbol = quoteSymbols[1]
+		}
+		end = line.indexOf(quoteSymbol, firstIndex + 1)
+		if (end < 0) {
+			addSliceToWords(line.slice(start), quoteSeparatedWords)
+			return quoteSeparatedWords
+		}
+		addSliceToWords(line.slice(start, firstIndex), quoteSeparatedWords)
+		start = end + 1
+		quoteSeparatedWords.push(line.slice(firstIndex + 1, end))
+	}
 }
 
 function getSplicedString(originalString, spliceIndex, spliceRemoved, spliceReplacement) {//should be moved to parseWord
